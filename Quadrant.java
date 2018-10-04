69b3d40b86b945f293ab085ff08775d2ab6df72a
Johan Åström <johaast@kth.se>
2018-10-04 08:01:16 +0200

    fooking works :fire: :sunglasses:

--- Quadrant.java
+++ Quadrant.java
@@ -16,7 +16,6 @@
     }
 
     public int dekompKvadrat(int m, int xPos, int yPos, int num, int vinkel) {
-        System.out.println("(xPos, yPos) + m : " + "(" + xPos + "," + yPos + ") " + m);
         int n = m/2;
         int h = n/2;
         num++;
@@ -46,7 +45,7 @@
 
 
             if(m >= 4) {
-                if(vinkel == 0 || vinkel == 2) {
+                if(vinkel == 2) {
                     num = dekompKvadrat(n, xPos - n + h, yPos + n - h, num, 2); //upp till höger
                     num = dekompKvadrat(n, xPos + n - h, yPos + n - h, num, 1); //ner till höger
                     num = dekompKvadrat(n, xPos + n - h, yPos - n + h, num, 2); //ner till vänster
@@ -56,10 +55,15 @@
                     num = dekompKvadrat(n, xPos + n - h, yPos + n - h, num, 1); //ner till höger
                     num = dekompKvadrat(n, xPos + n - h, yPos - n + h, num, 2); //ner till vänster
                     num = dekompKvadrat(n, xPos - n + h, yPos - n + h, num, 1); //upp till vänster
-                }  else {
+                }  else if(vinkel == 3) {
                     num = dekompKvadrat(n, xPos - n + h, yPos + n - h, num, 0); //upp till höger
                     num = dekompKvadrat(n, xPos + n - h, yPos + n - h, num, 3); //ner till höger
                     num = dekompKvadrat(n, xPos + n - h, yPos - n + h, num, 2); //ner till vänster
+                    num = dekompKvadrat(n, xPos - n + h, yPos - n + h, num, 3); //upp till vänster
+                } else{
+                    num = dekompKvadrat(n, xPos - n + h, yPos + n - h, num, 0); //upp till höger
+                    num = dekompKvadrat(n, xPos + n - h, yPos + n - h, num, 1); //ner till höger
+                    num = dekompKvadrat(n, xPos + n - h, yPos - n + h, num, 0); //ner till vänster
                     num = dekompKvadrat(n, xPos - n + h, yPos - n + h, num, 3); //upp till vänster
                 }
             }
@@ -73,7 +77,7 @@
         dekompKvadrat(m, m/2, m/2, 0, 2);
         for (int i = 0;i < m;i++ ) {
             for (int j = 0;j < m; j++ ) {
-                System.out.print(matrix[i][j] + " ");
+                System.out.format("%4d", matrix[i][j]);
             }
         System.out.println();
         }
